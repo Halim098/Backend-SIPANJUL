63781be4e4455d4177b05a244d3e322c402cad67
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-10-11 09:53:10 +0700

    update product model

--- Model/Product.go
+++ Model/Product.go
@@ -11,6 +11,9 @@ type Product struct {
     Name      string    `form:"name" json:"name" binding:"required"`
     Price     float64   `form:"price" json:"price" binding:"required"`
     Stock     int       `form:"stock" json:"stock" binding:"required"`
+	DIvision  string	`form:"division" json:"division" binding:"required"`
+	Location  string	`form:"location" json:"location" binding:"required"`
     CreatedAt time.Time `json:"created_at"`
     UpdatedAt time.Time `json:"updated_at"`
+	DeleteAt  time.Time `json:"delete_at"`
 }
