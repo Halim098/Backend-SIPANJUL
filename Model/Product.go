02d2abe3dac59ccd2ff3128b9df0842102b975c5
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-10-11 14:39:31 +0700

    add add produc

--- Model/Product.go
+++ Model/Product.go
@@ -1,6 +1,7 @@
 package Model
 
 import (
+	"Sipanjul/Database"
 	"time"
 
 	_ "gorm.io/gorm"
@@ -11,9 +12,20 @@ type Product struct {
     Name      string    `form:"name" json:"name" binding:"required"`
     Price     float64   `form:"price" json:"price" binding:"required"`
     Stock     int       `form:"stock" json:"stock" binding:"required"`
-	DIvision  string	`form:"division" json:"division" binding:"required"`
+	Division  string	`form:"division" json:"division" binding:"required"`
 	Location  string	`form:"location" json:"location" binding:"required"`
+	Active	  string	`form:"active" json:"active" binding:"required"`
     CreatedAt time.Time `json:"created_at"`
     UpdatedAt time.Time `json:"updated_at"`
 	DeleteAt  time.Time `json:"delete_at"`
 }
+
+func AddProduct (name string, price float64, stock int, division string, location string) error {
+	Product := Product{Name: name, Price: price, Stock: stock, Division: division, Location: location, Active: "true", CreatedAt: time.Now()}
+	err := Database.Database.Create(&Product)
+	if err.Error != nil {
+		return err.Error
+	}
+	return nil
+}
+
