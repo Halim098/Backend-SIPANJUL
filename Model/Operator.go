6ca33d6b7e4bd084898ddb97da276c202c2cb5eb
Muhammad Kurniawan Halim <halimml751@gmail.com>
2024-10-11 09:52:39 +0700

    update operator model

--- Model/Operator.go
+++ Model/Operator.go
@@ -2,13 +2,11 @@ package Model
 
 import (
 	"time"
-
-	_ "gorm.io/gorm"
 )
 
 type Operator struct {
     ID        uint      `gorm:"primarykey" json:"id"`
-    Username  string    `form:"username" json:"username" binding:"required"`
+    Name  string    `gorm:"unique;not null" form:"name" json:"name" binding:"required"` 
     Password  string    `form:"password" json:"password" binding:"required"`
     CreatedAt time.Time `json:"created_at"`
 }
