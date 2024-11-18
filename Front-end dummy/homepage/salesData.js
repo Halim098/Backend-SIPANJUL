7748aea2449a78620d8b4635e68885bcc6dcd62f
Halim <Halimml751@gmail.com>
2024-11-18 09:56:48 +0700

    update front end dummy

---
+++ Front-end dummy/homepage/salesData.js
@@ -0,0 +1,20 @@
+export const dailySales = {
+  currentValue: 1920000,
+  oldValue: 1650000,
+  percentage: 16.4,
+  isNegative: false,
+}
+
+export const weeklySales = {
+  currentValue: 13500000,
+  oldValue: 13440000,
+  percentage: 0.4,
+  isNegative: false,
+}
+
+export const monthlySales = {
+  currentValue: 57600000,
+  oldValue: 51250000,
+  percentage: 12.4,
+  isNegative: false,
+}
