1e03cf6ac9e53d1f57da483eefd1b2cc46dee217
Halim <Halimml751@gmail.com>
2024-11-18 10:10:19 +0700

    update seles detail model

--- Model/Sales_Detail.go
+++ Model/Sales_Detail.go
@@ -1,15 +1,25 @@
 package Model
 
 import (
+	"Sipanjul/Database"
+
 	_ "gorm.io/gorm"
 )
 
 type Sales_Detail struct {
     ID        uint      `gorm:"primarykey" json:"id"`
-    SalesID   uint      `json:"sales_id"` // Foreign key for Sales
-    ProdID    uint      `json:"prod_id"`  // Foreign key for Product
-    Quantity  int       `json:"quantity"`
-    Total     int       `json:"total"`
+    SalesID   uint      `json:"sales_id"`
+    ProdID    uint      `json:"prod_id" binding:"required"`
+    Quantity  int       `json:"quantity" binding:"required"`
+    Total     int       `json:"total" binding:"required"`
     
     Product   Product   `gorm:"foreignKey:ProdID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // Foreign key defined
+}
+
+func AddSalesDetail (data *Sales_Detail) error {
+	err := Database.Database.Create(&data)
+	if err.Error != nil {
+		return err.Error
+	}
+	return nil
 }
