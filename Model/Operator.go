aedcd2c7127e331d4ac8b8c2eef844403f9fd5e0
Halim <Halimml751@gmail.com>
2024-11-18 10:02:52 +0700

    update operator model

--- Model/Operator.go
+++ Model/Operator.go
@@ -6,12 +6,13 @@ import (
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 type Operator struct {
     ID        uint      `gorm:"primarykey" json:"id"`
-    Name  string    `gorm:"unique;not null" form:"name" json:"name" binding:"required"` 
-    Password  string    `form:"password" json:"password" binding:"required"`
+    Name  string    `gorm:"unique;not null" form:"name" json:"name"` 
+    Password  string    `form:"password" json:"password"`
     CreatedAt time.Time `json:"created_at"`
 }
 
@@ -24,31 +25,48 @@ func (o *Operator) ValidatePassword(password string) error {
 	return bcrypt.CompareHashAndPassword([]byte(o.Password), []byte(password))
 }
 
-func FindOperator (name, password string) (string,error) {
-	var Operator Operator
-	err := Database.Database.Raw("SELECT * FROM operators WHERE name = ? AND password = ?", name, password).Scan(&Operator)
+func (o *Operator) BeforeSave(*gorm.DB) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(o.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	o.Password = string(hashedPassword)
+	return nil
+}
+
+func (o *Operator) Save() error {
+	err := o.BeforeSave(Database.Database)
+	if err != nil {
+		return err
+	}
+
+	result, _ := FindOperatorByName(o.Name)
+	if result.Name == o.Name {
+		return errors.New("username already exists")
+	}
+
+	o.CreatedAt = time.Now()
+
+	err = Database.Database.Exec("INSERT INTO operators (name, password, created_at) VALUES (?, ?, ?)", o.Name, o.Password, o.CreatedAt).Error
+	if err != nil {
+		return errors.New("failed to create user")
+	}
+	
+	return nil
+}
+
+func FindOperatorByName(username string) (Operator, error) {
+	var opr Operator
+	err := Database.Database.Raw("SELECT * FROM operators WHERE name = ?", username).Scan(&opr)
 	if err.Error != nil {
-		return "", err.Error
+		return Operator{}, err.Error
 	}
 
 	if err.RowsAffected == 0 {
-		return "", errors.New("user not found")
+		return Operator{}, errors.New("user not found")
 	}
 
-	return Operator.Name, nil
+	return opr, nil
 }
 
-func GetOperator () ([]Operator,error) {
-    var Operator []Operator
-    
-    err := Database.Database.Raw("SELECT * FROM operators)").Scan(&Operator)
-    if err.Error != nil {
-        return Operator, err.Error
-    }
-
-    if err.RowsAffected == 0 {
-        return Operator, errors.New("user not found")
-    }
-
-    return Operator, nil
-}
