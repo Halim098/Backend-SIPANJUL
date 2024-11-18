38de9fea942a757500a20767c753bb1cf9dce61b
Halim <Halimml751@gmail.com>
2024-11-18 10:25:28 +0700

    refactor main function to use HTTP handler for routing

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"Sipanjul/Database"
 	"Sipanjul/Router"
 	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
 	// Add logger package for GORM
 )
 
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
     loadEnv()
     Database.Connect()
 	// Database.Database.AutoMigrate(&Model.Product{}, &Model.Operator{},  &Model.Sales{}, &Model.Sales_Detail{})
 
-	Router.SetupRouter().Run("0.0.0.0:8050")
+	// Router.SetupRouter().Run("0.0.0.0:8050")
+	router := Router.SetupRouter()
+	router.ServeHTTP(w, r)
 }
 
 func loadEnv() {
