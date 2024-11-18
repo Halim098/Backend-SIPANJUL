263b5cfba001f3f4a13aced1f736409c98f4a769
Halim <Halimml751@gmail.com>
2024-11-18 11:04:53 +0700

    update database code

--- Database/Database.go
+++ Database/Database.go
@@ -10,21 +10,26 @@ import (
 var Database *gorm.DB
 
 func Connect() {
+    host := os.Getenv("DB_HOST")
+    username := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    databaseName := os.Getenv("DB_NAME")
+    port := os.Getenv("DB_PORT")
 
-	var err error
+    if host == "" || username == "" || password == "" || databaseName == "" || port == "" {
+        panic("Database configuration is missing required environment variables")
+    }
 
-	host := os.Getenv("DB_HOST")
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	databaseName := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
+    dsn := fmt.Sprintf(
+        "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos",
+        host, username, password, databaseName, port,
+    )
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Africa/Lagos", host, username, password, databaseName, port)
+    var err error
     Database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        panic("Failed to connect to database: " + err.Error())
+    }
 
-	if err != nil {
-		panic("Failed to connect to database!")
-	}
-
-	fmt.Println("Database connection established")
+    fmt.Println("Database connection established")
 }
