a389ea640bc6ccd9240853f2a977de5e64b01a5b
Halim <Halimml751@gmail.com>
2024-11-20 13:32:05 +0700

    add product report

--- Model/Product_Report.go
+++ Model/Product_Report.go
@@ -1,5 +1,19 @@
 package Model
 
 import (
+	"Sipanjul/Database"
+	"errors"
+	"time"
+
 	_ "gorm.io/gorm"
 )
+
+type Product_repost struct {
+	ID        	uint      	`gorm:"primarykey" json:"id"`
+	ProdID	    uint      	`json:"prod_id"`
+	Quantity  	int       	`json:"quantity" binding:"required"`
+	Action		string    	`json:"action" binding:"required"`
+
+	Product     Product     `gorm:"foreignKey:ProdID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // Foreign key defined
+}
+
