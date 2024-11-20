01b504779527f065b2c7e30e2f234d7000973777
Halim <Halimml751@gmail.com>
2024-11-20 14:40:45 +0700

    add AddProductReport

--- Model/Product_Report.go
+++ Model/Product_Report.go
@@ -2,18 +2,28 @@ package Model
 
 import (
 	"Sipanjul/Database"
-	"errors"
 	"time"
 
 	_ "gorm.io/gorm"
 )
 
-type Product_repost struct {
+var query string = "SELECT r.id, p.name, r.quantity, r.action, r.description r.date FROM product_reports r JOIN products p ON r.prod_id = p.id"
+
+type ProductReport struct {
 	ID        	uint      	`gorm:"primarykey" json:"id"`
 	ProdID	    uint      	`json:"prod_id"`
 	Quantity  	int       	`json:"quantity" binding:"required"`
 	Action		string    	`json:"action" binding:"required"`
+	Description	string		`json:"description" binding:"required"`
+	Date 		time.Time 	`json:"date"`
 
 	Product     Product     `gorm:"foreignKey:ProdID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // Foreign key defined
 }
 
+func AddProductReport (data *ProductReport) error {
+	err := Database.Database.Create(&data)
+	if err.Error != nil {
+		return err.Error
+	}
+	return nil
+}
