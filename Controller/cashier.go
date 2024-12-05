eed854a3c92fc2fa8f73416a24a9829b7052b8eb
Halim <Halimml751@gmail.com>
2024-12-05 08:33:11 +0700

    add stock management in Checkout process

--- Controller/cashier.go
+++ Controller/cashier.go
@@ -13,10 +13,31 @@ func Checkout(oprid uint, total int, detail *[]Model.Sales_Detail) error {
 
 	for _, v := range *detail {
 		v.SalesID = data.ID
+		prodData, err := GetProductByID(v.ProdID)
+		if err != nil {
+			return err
+		}
+		v.StockAwal = prodData.Stock
+		v.StockAkhir = v.StockAwal - v.Quantity
+
 		err = Model.AddSalesDetail(&v)
 		if err != nil {
 			return err
 		}
+
+		updated := Model.Product{
+			Division: prodData.Division,
+			Name: prodData.Name,
+			Price: prodData.Price,
+			Stock: -v.Quantity,
+			UpdatedAt: prodData.UpdatedAt,
+		}
+
+		err = Model.UpdateProduct(&prodData, &updated)
+		if err != nil {
+			return err
+		}
+	
 	}
 	return nil
 }
