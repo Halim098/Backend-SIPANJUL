26fa68aeaf5190379abefa8d4fb6e7e1d47fec39
Halim <Halimml751@gmail.com>
2024-12-17 21:23:12 +0700

    update login

--- Service/Login.go
+++ Service/Login.go
@@ -31,8 +31,9 @@ func Login(c *gin.Context)  {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"status":"success", 
-		"message": map[string]string{"token": token},
+		"status":"success",
+		"message":"Login Berhasil", 
+		"data": map[string]string{"token": token},
 	})
 }
 
